5c72579cbcfafec930f94603baf95476f0970398
Patryk Dagiel <patryk.dagiel@gmail.com>
2017-02-07 14:10:30 +0100

    Version 0.0.7  Euler project: 5 problems solved

---
+++ src/test/java/Euler_Project_Alghoritms/Largest_PalindromeTest.java
@@ -0,0 +1,17 @@
+package Euler_Project_Alghoritms;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dagiel on 07/02/2017.
+ */
+public class Largest_PalindromeTest {
+
+    @Test
+    public void testMaxNumber() {
+    }
+
+
+}
